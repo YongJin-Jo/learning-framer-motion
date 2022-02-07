5c2c7848a5ca1b46df8fd3afab2222ca22d347ee
artinc5233 <artinc5233@gmail.com>
2022-02-07 10:40:00 +0900

    motion에 대한 이해 animate 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
+import { motion } from 'framer-motion';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Warrper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+
+  background-color: white;
+`;
+
+const myVaras = {
+  start: { scale: 0 },
+  end: {
+    scale: 1,
+    borderRadius: '20px',
+    rotateZ: 360,
+    transition: { type: 'spring', dely: 0.5 },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Warrper className="App">
+      <Box variants={myVaras} initial="start" animate="end"></Box>
+    </Warrper>
   );
 }
 
---
+++ src/asset/css/theme/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: '#f368e0',
+  boardColor: '#DADFE9',
+  cardColor: 'white',
+};
